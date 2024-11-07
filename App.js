6607942522cc44de558fb01659669017a400b752
doudou <dushuying1996@gmail.com>
2024-11-06 21:54:13 -0800

    setup app navigation structure

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Import screen components
+import HomeScreen from './Components/HomeScreen';
+import MapScreen from './Components/MapScreen';
+import PostScreen from './Components/PostScreen';
+import LeaderboardScreen from './Components/LeaderboardScreen';
+import ProfileScreen from './Components/ProfileScreen';
+import MyBreedScreen from './Components/MyBreedScreen';
+
+
+
+// Create navigation stacks
+const HomeStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="MyPosts" component={HomeScreen} options={{ title: 'My Posts' }} />
+      <HomeStack.Screen name="MyBreed" component={MyBreedScreen} options={{ title: 'My Breed' }} />
+    </HomeStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeStackScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ title: 'HotSpots' }}
+        />
+        <Tab.Screen
+          name="Post"
+          component={PostScreen}
+          options={{ title: 'New Post' }}
+        />
+        <Tab.Screen
+          name="Leaderboard"
+          component={LeaderboardScreen}
+          options={{ title: 'Leader Board' }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: 'My Profile' }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
