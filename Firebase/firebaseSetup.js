c67abb964ffc6598db573e5bdf9da8bc36827885
nina <lui.n@northeastern.edu>
2024-11-06 23:57:23 -0800

    added firebase connection

---
+++ Firebase/firebaseSetup.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_apdId,
+  measurementId: process.env.EXPO_PUBLIC_measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
