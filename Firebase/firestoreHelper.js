644286ccd829f5f1f89bd45c50ee4693d0c6ce02
nina <lui.n@northeastern.edu>
2024-11-07 10:01:14 -0800

    created firestore helper for crud operations

---
+++ Firebase/firestoreHelper.js
@@ -0,0 +1,64 @@
+import { database } from './firebaseSetup';
+import { collection, addDoc, doc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore';
+
+// Create a new document in the specified collection
+export async function writeToDB(data, collectionName) {
+    try {
+        await addDoc(collection(database, collectionName), data);
+    } catch (error) {
+        console.error('Error adding document: ', error);
+    }
+}
+
+// Get a specific document from the specified collection
+export async function getDocument(docID, collectionName) {
+    try {
+        const docRef = doc(database, collectionName, docID);
+        const docSnap = await getDoc(docRef);
+        if (!docSnap.exists()) {
+            console.log('No such document!');
+            return;
+        } else {
+            return docSnap.data();
+        }
+    } catch (error) {
+        console.log('Error getting document:', error);
+    }
+}
+
+// Get all documents from the specified collection
+export async function getAllDocuments(collectionName) {
+  try {
+      const querySnapshot = await getDocs(collection(database, collectionName));
+      const data = [];
+      if (querySnapshot.empty) {
+          console.log("No documents found in ", collectionName);
+          return data;
+      }
+      querySnapshot.forEach((docSnapshot) => {
+          data.push(docSnapshot.data());
+      });
+      return data;
+  }
+  catch (error) {
+      console.log("Error getting documents: ", error);
+  }
+}
+
+// Update a document in the specified collection
+export async function updateDB(docID, data, collectionName) {
+    try {
+        await updateDoc(doc(database, collectionName, docID), data);
+    } catch (error) {
+        console.error('Error updating document: ', error);
+    }
+}
+
+// Delete a specific document from the specified collection
+export async function deleteFromDB(docID, collectionName) {
+    try {
+        await deleteDoc(doc(database, collectionName, docID));
+    } catch (error) {
+        console.error('Error deleting document: ', error);
+    }
+}
