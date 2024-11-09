1ace3a533e7ef830513a0daebfe1e0c7cad34f3c
nina <lui.n@northeastern.edu>
2024-11-09 12:45:21 -0800

    added basic layout for profile screen

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
 import { auth } from '../Firebase/firebaseSetup';
 import { getDocument } from '../Firebase/firestoreHelper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const ProfileScreen = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -27,17 +28,76 @@ const ProfileScreen = ({ navigation }) => {
     }, []);
 
     return (
-        <View >
-            <Text >
+        <View style={styles.container}>
+            {/* Button for notification settings */}
+            <View style={styles.notificationContainer}>
+                <Pressable>
+                    <MaterialCommunityIcons name="bell" size={24} color="blue" />
+                </Pressable>
+            </View>
+
+
+            {/* User image */}
+            {/* Placeholder, need to fetch from database */}
+            <View style={styles.userImageIcon}>
+            </View>
+
+            {/* User name */}
+            <Text style={styles.boldText}>
                 Welcome, {username}
             </Text>
+
+            {/* Breed collection progress */}
+            {/* Placeholder - making reusable component */}
+            <Text>
+                0/120 Breeds Collected
+            </Text>
+
+            {/* Navigate to breed collection */}
             <Pressable
                 onPress={() => navigation.navigate('MyBreed')}
             >
-                <Text style={{ color: 'blue' }}>Go to My Breed</Text>
+                <Text style={{ color: 'blue' }}>Go to My Breed Collection</Text>
             </Pressable>
-        </View >
+
+            {/* Top 3 breeds */}
+            {/* Placeholder - making reusable component */}
+            <Text style={styles.boldText}>
+                Top Breeds
+            </Text>
+            <View style={styles.topBreedIconsContainer}>
+            </View>
+        </View>
+
     );
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    notificationContainer: {
+        alignSelf: 'flex-end',
+        padding: 10,
+    },
+    userImageIcon: {
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    topBreedIconsContainer: {
+        flexDirection: 'row',
+        padding: 10,
+    },
+    boldText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+}); 
