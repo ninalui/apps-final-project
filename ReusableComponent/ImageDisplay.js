a61e1d5236379f5e9ff2920c92c5c39a004873eb
nina <lui.n@northeastern.edu>
2024-11-10 10:30:09 -0800

    fixed bug with profile not re-rendering with new picture

--- ReusableComponent/ImageDisplay.js
+++ ReusableComponent/ImageDisplay.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, Text } from "react-native";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 export default function ImageDisplay({ imageUri, displayStyle }) {
   const [imageUrl, setImageUrl] = useState(null);
 
   useEffect(() => {
-    const storage = getStorage();
-    const imageRef = ref(storage, imageUri);
-    getDownloadURL(imageRef)
-      .then((url) => {
-        setImageUrl(url);
-      })
-      .catch((error) => {
-        console.error("Error fetching image: ", error);
-      });
-  }, []);
+    if (imageUri) {
+      const fetchImageUrl = async () => {
+        try {
+          const storage = getStorage();
+          const imageRef = ref(storage, imageUri);
+          const url = await getDownloadURL(imageRef);
+          setImageUrl(url);
+        } catch (error) {
+          console.error("Error fetching image: ", error);
+        }
+    };
+    fetchImageUrl();
+  }
+  }, [imageUri]);
 
   return (
     <Image
