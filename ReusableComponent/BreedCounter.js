4a61381c7a8b1b8b8e20a71345da8539d0d60926
nina <lui.n@northeastern.edu>
2024-11-10 01:33:57 -0800

    added fetching user's breed count from database

--- ReusableComponent/BreedCounter.js
+++ ReusableComponent/BreedCounter.js
@@ -1,11 +1,29 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { auth } from '../Firebase/firebaseSetup'
+import { getAllDocuments } from '../Firebase/firestoreHelper'
 
 export default function BreedCounter() {
   const [breedCount, setBreedCount] = React.useState(0);
+  const currentUser = auth.currentUser.uid;
+
+  // Fetch breed count from database
+  useEffect(() => {
+    const fetchBreedCount = async () => {
+      try {
+        const breedSubcollectionPath = `users/${currentUser}/breeds`;
+        const breeds = await getAllDocuments(breedSubcollectionPath);
+        setBreedCount(breeds.length);
+      } catch (error) {
+        console.error('Error fetching breed count: ', error);
+      }
+    }
+    fetchBreedCount();
+    }, []);
 
   return (
     <View style={styles.container}>
+      {/* API for breed detection uses database that has 120 breeds */}
       <Text>{breedCount} out of 120 breeds collected</Text>
     </View>
   )
