589d17342b63444ebace907c9ffe4b799c5c99e6
nina <lui.n@northeastern.edu>
2024-11-10 23:30:55 -0800

    fetched images for each breed in MyBreedScreen from firestore, updated breedCollection state with image URLs to display

--- Screens/MyBreedScreen.js
+++ Screens/MyBreedScreen.js
@@ -1,9 +1,56 @@
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { auth } from '../Firebase/firebaseSetup';
+import { database } from '../Firebase/firebaseSetup';
+import { collection, query, where, getDocs, limit } from 'firebase/firestore';
 
-const MyBreedScreen = () => {
+import BreedCounter from '../ReusableComponent/BreedCounter';
+import BreedIcon from '../ReusableComponent/BreedIcon';
+
+const MyBreedScreen = ({ route }) => {
+    const { breeds } = route.params;
+    const [breedCollection, setBreedCollection] = useState(breeds);
+    const currentUser = auth.currentUser.uid;
+
+    // Fetch images for each breed
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                let breedImages = [...breedCollection];
+                for (let i = 0; i < breedImages.length; i++) {
+                    const postsQuery = query(
+                        collection(database, `users/${currentUser}/posts`),
+                        where('breed', '==', breedImages[i].breedName),
+                        limit(1)
+                    );
+                    const imageSnapshot = await getDocs(postsQuery);
+                    if (!imageSnapshot.empty) {
+                        const image = imageSnapshot.docs[0].data().imageUrl;
+                        breedImages[i].imageUrl = image;
+                    } else {
+                        breedImages[i].imageUrl = '';
+                    }
+                }
+                setBreedCollection(breedImages);
+            } catch (error) {
+                console.error('Error fetching images for collection: ', error);
+            }
+        };
+        if (breedCollection.length > 0) {
+            fetchImages();
+        }
+    }, [breedCollection]);
+
     return (
         <View>
-            <Text>My Breed Screen</Text>
+            <BreedCounter />
+            <View>
+                {breedCollection.map((breed) => {
+                    return (
+                        <BreedIcon key={breed.breedName} breedImage={breed.imageUrl} breedName={breed.breedName} />
+                    );
+                })}
+            </View>
         </View>
     );
 };
